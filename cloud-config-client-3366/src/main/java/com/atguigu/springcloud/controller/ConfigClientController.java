0789019f417d029403d8475ef061b5a5fcb65a14
吴超 <694679727@qq.com>
2020-04-16 21:26:56 +0800

    2020.4.16

--- cloud-config-client-3366/src/main/java/com/atguigu/springcloud/controller/ConfigClientController.java
+++ cloud-config-client-3366/src/main/java/com/atguigu/springcloud/controller/ConfigClientController.java
@@ -1,4 +1,4 @@
-package com.atguigu.springcloud.config;
+package com.atguigu.springcloud.controller;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
@@ -15,11 +15,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RefreshScope
 public class ConfigClientController {
 
+    @Value("${server.port}")
+    private String serverPort;
+
     @Value("${config.info}")
     private String configInfo;
 
     @GetMapping("/configInfo")
     public String getConfigInfo(){
-        return configInfo;
+        return "server.port = " + serverPort + "\t configInfo=" + configInfo;
     }
 }
